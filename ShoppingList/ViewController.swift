d0cb6a60afc6e2257bbae7b7a85adbe033a62dab
temitayo65 <iloritemitayo65@gmail.com>
2022-09-10 23:12:43 +0100

    First Version

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -7,13 +7,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var shoppingList = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "MY SHOPPING LIST"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(clearTable))
+        let addButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
+        let shareButtonItem = UIBarButtonItem(title: "Share", style: .plain, target: self, action: #selector(shareButtonTapped))
+        navigationItem.rightBarButtonItems = [addButtonItem, shareButtonItem]
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "shopidentifier", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
     }
 
+    @objc func addItem(){
+        let ac = UIAlertController(title: "What Item do you want to add?", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        let submitAction = UIAlertAction(title: "Submit", style: .default){[weak self, weak ac] _ in
+            guard let addedItem = ac?.textFields?[0].text else{return}
+            self?.submit(addedItem)
+        }
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+        
+    }
+    
+    func submit(_ addedItem: String){
+        shoppingList.insert(addedItem, at: 0)
+        let indexPath = IndexPath(row: 0, section: 0)
+        self.tableView.insertRows(at: [indexPath], with: .automatic)
+        return 
+    }
 
+    @objc func clearTable(){
+        shoppingList.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    @objc func shareButtonTapped(){
+        let list = shoppingList.joined(separator: "\n")
+        let ac = UIActivityViewController(activityItems: [list], applicationActivities: nil)
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
 }
 
